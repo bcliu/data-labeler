4fa606cec00433aefb175ea0fa8620639eb6fdfb
bcliu <leo168haha@gmail.com>
2015-07-29 00:30:29 -0700

    Added queries

--- index.js
+++ index.js
@@ -15,8 +15,57 @@ app.use(multer({ dest: './uploads/' }).single('file'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'data_labeler'
+});
+
 app.get('/', function (req, res) {
-	res.send('hello world');
+    /* Pick 10% of messages out of each category */
+    var clusterCounts = [];
+    var clusterAlreadyLabeledCounts = [];
+
+    connection.query('SELECT distinct(cluster) from data', function(err, rows, fields) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        rows.forEach(function (val, index, arr) {
+            connection.query('select count(*) from data where cluster = "' + val.cluster + '"', function (err, result) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                clusterCounts[val.cluster] = result[0]['count(*)'];
+            });
+        });
+
+        rows.forEach(function (val, index, arr) {
+            connection.query('select count(*) from data where cluster = "' + val.cluster + '" and is_spam != NULL', function (err, result) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                clusterAlreadyLabeledCounts[val.cluster] = result[0]['count(*)'];
+            });
+        });
+    });
+
+    /* This is bad */
+    setTimeout(function () {
+        clusterCounts.forEach(function (val, index, arr) {
+            var limit = Math.floor(val * 0.01) - clusterAlreadyLabeledCounts[index];
+            if (limit <= 0) {
+                return;
+            }
+            connection.query('select * from data where cluster = "' + index + '" and is_spam is NULL limit ' + limit, function (err, result) {
+                res.send(result);
+            });
+        });
+    }, 3000);
 });
 
 app.get('/import', function (req, res) {
@@ -25,13 +74,6 @@ app.get('/import', function (req, res) {
 
 /* Import file */
 app.post('/import', function (req, res) {
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'data_labeler'
-    });
-
     fs.createReadStream(req.file.path).pipe(csv()).on('data', function (data) {
         connection.connect();
         var query = connection.query('INSERT INTO data SET ?', data, function (err, result) {
